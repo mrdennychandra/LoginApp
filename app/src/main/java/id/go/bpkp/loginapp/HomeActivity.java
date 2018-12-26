028a0337cb694df8e62dd5d1b67d06242c030970
mrdennychandra <dennyc.dev@gmail.com>
2018-12-26 21:42:41 +0700

    Adding BottomNav & FAB

--- app/src/main/java/id/go/bpkp/loginapp/HomeActivity.java
+++ app/src/main/java/id/go/bpkp/loginapp/HomeActivity.java
@@ -1,47 +1,57 @@
 package id.go.bpkp.loginapp;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import id.go.bpkp.loginapp.fragment.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
-    SharedPreferences setting;
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_dashboard:
+
+                    return true;
+                case R.id.navigation_notifications:
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        setting = PreferenceManager
-                .getDefaultSharedPreferences(HomeActivity.this);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new HomeFragment());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_home,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if(id == R.id.action_logout){
-            SharedPreferences.Editor editor = setting.edit();
-            editor.clear();
-            editor.commit();
-            Intent intent =
-                    new Intent(HomeActivity.this,
-                            MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        if(id == R.id.action_setting){
-            //
-        }
-        return true;
-    }
 }
